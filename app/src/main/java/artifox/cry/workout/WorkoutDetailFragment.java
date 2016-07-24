0fd96dc5bce5c48616e9ffae96d671f2a9db1eb1
Artsiom Koptsiukh <artsiom.koptsiukh@gmail.com>
2016-07-24 15:44:34 +0300

    Add WorkoutListFragment ot MainActivity

---
+++ app/src/main/java/artifox/cry/workout/WorkoutDetailFragment.java
@@ -0,0 +1,41 @@
+package artifox.cry.workout;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class WorkoutDetailFragment extends Fragment {
+    private long workoutId;
+
+    public void setWorkoutId(long id) {
+        this.workoutId = id;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        View view = getView();
+
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            Workout workout = Workout.WORKOUTS[(int)workoutId];
+
+            title.setText(workout.getName());
+            description.setText(workout.getDescription());
+        }
+
+    }
+}
---
+++ app/src/main/java/artifox/cry/workout/WorkoutListFragment.java
@@ -0,0 +1,26 @@
+package artifox.cry.workout;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+public class WorkoutListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String[] names = new String[Workout.WORKOUTS.length];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = Workout.WORKOUTS[i].getName();
+        }
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
+        setListAdapter(listAdapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
